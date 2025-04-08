aa0cbf190dbdd40b9b7ec8ad86a6a333662f9682
tomason182 <elotro182@hotmail.com>
2025-04-08 20:14:16 -0300

    modify: style and max width of header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,20 @@ import PropTypes from "prop-types";
 export default function Header({ name }) {
   return (
     <header className={styles.header}>
-      <div className={styles.hostelName}>
-        <h1>{name}</h1>
-      </div>
-      <div className={styles.options}>
-        <select name="language" id="language">
-          <option value="es">Español</option>
-          <option value="en">English</option>
-        </select>
-        <select name="currency" id="currency">
-          <option value="usd">USD</option>
-          <option value="ars">ARS</option>
-        </select>
+      <div className={styles.content}>
+        <div className={styles.hostelName}>
+          <h1>{name}</h1>
+        </div>
+        <div className={styles.options}>
+          <select name="language" id="language">
+            <option value="es">Español</option>
+            <option value="en">English</option>
+          </select>
+          <select name="currency" id="currency">
+            <option value="usd">USD</option>
+            <option value="ars">ARS</option>
+          </select>
+        </div>
       </div>
     </header>
   );
